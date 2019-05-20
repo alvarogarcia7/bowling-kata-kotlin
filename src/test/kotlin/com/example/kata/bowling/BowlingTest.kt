2f964b7694f878380d55c0aea9e053de28f65a77
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-05-20 22:10:33 +0200

    Refactor: create parametrized for no pins are knocked down

--- src/test/kotlin/com/example/kata/bowling/BowlingTest.kt
+++ src/test/kotlin/com/example/kata/bowling/BowlingTest.kt
@@ -1,33 +1,40 @@
 package com.example.kata.bowling
 
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is
 import org.hamcrest.core.Is.`is`
-import org.junit.Test
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
-class BowlingTest{
 
-    @Test
-    fun total_score_when_no_pins_are_knocked_down(){
-        assertThat(0, `is`(calculateScore("--|--|--|--|--|--|--|--|--|--||")))
+class BowlingTest {
+
+    @ParameterizedTest(name = "No Pins are knocked down: {0}")
+    @CsvSource(
+        "--|--|--|--|--|--|--|--|--|--||"
+    )
+    fun total_score_when_no_pins_are_knocked_down(gameRepresentation: String) {
+        assertThat(calculateScore(gameRepresentation), Is.`is`(0))
     }
 
     @Test
-    fun total_score_for_one_frame_when_no_pins_are_knocked_down(){
+    fun total_score_for_one_frame_when_no_pins_are_knocked_down() {
         assertThat(0, `is`(calculateScore("--")))
     }
 
     @Test
-    fun `total score for one frame when one pin is knocked down`(){
+    fun `total score for one frame when one pin is knocked down`() {
         assertThat(1, `is`(calculateScore("1-")))
     }
 
     @Test
-    fun `total score for one frame when one pin is knocked down after a miss`(){
+    fun `total score for one frame when one pin is knocked down after a miss`() {
         assertThat(1, `is`(calculateScore("-1")))
     }
 
     @Test
-    fun `total score for one frame when eight pins are knocked down`(){
+    fun `total score for one frame when eight pins are knocked down`() {
         assertThat(8, `is`(calculateScore("71")))
     }
 
