bc115c02d0a360db8ac82feaeffe79b5f52cba05
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-05-20 20:52:01 +0200

    Refactor: remove var assignment (from 13->12)

--- src/main/kotlin/com/example/kata/bowling/Game.kt
+++ src/main/kotlin/com/example/kata/bowling/Game.kt
@@ -2,19 +2,17 @@ package com.example.kata.bowling
 
 class Game(private val gameRepresentation: String) {
     fun score(): Int {
-        var result = 0
-        result += scoreOfASingleThrow(gameRepresentation[0])
-        result += scoreOfASingleThrow(gameRepresentation[1])
-        return result
+
+        return scoreOfASingleThrow(gameRepresentation[0]) +
+                scoreOfASingleThrow(gameRepresentation[1])
     }
 
     private fun scoreOfASingleThrow(throwRepresentation: Char): Int {
-        var result = 0
-        try {
-            result = Integer.parseInt(throwRepresentation.toString())
+        return try {
+            Integer.parseInt(throwRepresentation.toString())
         } catch (e: Exception) {
-            //no op
+            0
         }
-        return result
+
     }
 }
