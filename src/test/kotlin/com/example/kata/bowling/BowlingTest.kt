909abb81b62feebbeff3ef5a5d02e829c56f2eb3
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-05-20 22:15:20 +0200

    Refactor: remove unnecessary imports

--- src/test/kotlin/com/example/kata/bowling/BowlingTest.kt
+++ src/test/kotlin/com/example/kata/bowling/BowlingTest.kt
@@ -2,8 +2,6 @@ package com.example.kata.bowling
 
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.Is
-import org.hamcrest.core.Is.`is`
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
