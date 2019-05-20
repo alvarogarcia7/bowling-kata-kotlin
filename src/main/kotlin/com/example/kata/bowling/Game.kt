982b04da17a74effb4f7623d1d0f2f21a83e1236
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-05-20 19:59:47 +0200

    Green: score the first frame, based on the first throw only (scaffold)

--- src/main/kotlin/com/example/kata/bowling/Game.kt
+++ src/main/kotlin/com/example/kata/bowling/Game.kt
@@ -1,7 +1,11 @@
 package com.example.kata.bowling
 
-class Game(gameRepresentation: String) {
+class Game(private val gameRepresentation: String) {
     fun score(): Int {
-        return 0
+        return try {
+            Integer.parseInt(gameRepresentation[0].toString())
+        } catch (e: Exception) {
+            0
+        }
     }
 }
--- src/test/kotlin/com/example/kata/bowling/BowlingTest.kt
+++ src/test/kotlin/com/example/kata/bowling/BowlingTest.kt
@@ -7,9 +7,21 @@ import org.junit.Test
 class BowlingTest{
 
     @Test
-    fun total_score_when_no_pins_knocked_down(){
+    fun total_score_when_no_pins_are_knocked_down(){
         assertEquals(0, calculateScore("--|--|--|--|--|--|--|--|--|--||"))
     }
+
+    @Test
+    fun total_score_for_one_frame_when_no_pins_are_knocked_down(){
+        assertEquals(0, calculateScore("--"))
+    }
+
+    @Test
+    fun `total score for one frame when one pin is knocked down`(){
+        assertEquals(1, calculateScore("1-"))
+    }
+
+
 
     private fun calculateScore(gameRepresentation: String): Int {
         return Game(gameRepresentation).score()
